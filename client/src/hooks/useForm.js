e2a76cfd30b3efbfec0b3999ed7e0cac6d5befe0
Jason Duncan <jasonduncan@Jasons-MacBook-Pro.local>
2020-07-18 12:59:24 -0400

    plants display and form works

--- client/src/hooks/useForm.js
+++ client/src/hooks/useForm.js
@@ -1,1 +1,28 @@
-// write your custom hook here to control your checkout form
+import { useLocalStorage } from './useLocalStorage';
+
+export const useForm = (key, initialValue, callback) => {
+	const [values, setValues] = useLocalStorage(key, {
+		...initialValue,
+	});
+
+	const handleChanges = (e) => {
+		setValues({
+			...values,
+			[e.target.name]: e.target.value,
+		});
+	};
+
+	const clearForm = (e) => {
+		e.preventDefault();
+		setValues({
+			...initialValue,
+		});
+	};
+
+	const handleSubmit = (e) => {
+		e.preventDefault();
+		callback();
+	};
+
+	return [values, handleChanges, handleSubmit, clearForm];
+};
